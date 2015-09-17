dbb054a9c994590ccc723447481cf01532532c44
Amanpreet Singh <apsdehal@gmail.com>
2015-09-18 01:22:03 +0530

    Tests: Shift to grunt-contrib-qunit

--- build/tasks/options/qunit.js
+++ build/tasks/options/qunit.js
@@ -5,23 +5,25 @@ module.exports = function( grunt ) {
 return {
 	options: {
 		timeout: 60000,
-		"--web-security": "no",
-		coverage: {
-			baseUrl: ".",
-			src: [
-				"js/**/*.js",
-				"!js/jquery.tag.inserter.js",
-				"!js/requirejs.config.js"
-			],
-			instrumentedFiles: "temp/",
-			htmlReport: "_tests/reports/coverage",
-			lcovReport: "_tests/reports/lcov",
-			linesThresholdPct: 0
-		}
+		"--web-security": "no"
+
+		// coverage: {
+		// 	baseUrl: ".",
+		// 	src: [
+		// 		"js/**/*.js",
+		// 		"!js/jquery.tag.inserter.js",
+		// 		"!js/requirejs.config.js"
+		// 	],
+		// 	instrumentedFiles: "temp/",
+		// 	htmlReport: "_tests/reports/coverage",
+		// 	lcovReport: "_tests/reports/lcov",
+		// 	linesThresholdPct: 0
+		// }
 	},
 
 	http: {
 		options: {
+			inject: false,
 			urls: ( function() {
 				var allSuites, patterns, paths,
 					testDirs = [ "unit", "integration", "css" ],
