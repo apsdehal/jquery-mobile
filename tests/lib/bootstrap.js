4341fa0842b8999b91b04f7768bbd22b7734ffce
Amanpreet Singh <apsdehal@gmail.com>
2015-09-25 06:01:08 +0545

    Tests: Add base bootstrap lib

---
+++ tests/lib/bootstrap.js
@@ -0,0 +1,158 @@
+( function() {
+
+		var widgets = [
+		// Main Widgets
+		"accordion",
+		"addFirstLastClasses",
+		"collapsible",
+		"collapsibleSet",
+		"controlgroup",
+		"dialog",
+		"enhancer",
+		"enhancer.backcompat",
+		"enhancer.widgetCrawler",
+		"filterable",
+		"fixedToolbar",
+		"fixedToolbar.workarounds",
+		"listview",
+		"listview.autodividers",
+		"listview.hidedividers",
+		"loader",
+		"navbar",
+		"page",
+		"page.dialog",
+		"pagecontainer",
+		"panel",
+		"popup",
+		"popup.arrow",
+		"table",
+		"tabs.ajax",
+		"table.reflow",
+		"table.columntoggle",
+		"toolbar",
+		"widget.backcompat",
+		"widget.theme",
+		// Form Widgets
+		"forms/autogrow",
+		"forms/button",
+		"forms/checkboxradio",
+		"forms/clearButton",
+		"forms/flipswitch",
+		"forms/rangeslider",
+		"forms/rangeslider.backcompat",
+		"forms/reset",
+		"forms/select",
+		"forms/select.custom",
+		"forms/slider",
+		"forms/slider.backcompat",
+		"forms/slider.tooltip",
+		"forms/textinput",
+		"forms/textinput.backcompat"
+	];
+
+	var events = [
+		"navigate",
+		"orientationchange",
+		"scroll",
+		"throttledresize",
+		"touch"
+	];
+
+	function getPath( dep ) {
+		for ( var i = 0; i < widgets.length; i++ ) {
+			if ( widgets[i] === dep ) {
+				return "widgets/" + dep;
+			}
+		}
+
+		for ( var i = 0; i < events.length; i++ ) {
+			if ( events[i] === dep ) {
+				return "events/" + dep;
+			}
+		}
+		return dep;
+	}
+
+	function fixPaths( deps ) {
+		for ( var i = 0; i < deps.length; i++ ) {
+			deps[ i ] = getPath( deps[ i ] );
+		}
+
+		return deps;
+	}
+
+	function requireModules( dependencies, callback, modules ) {
+		if ( dependencies.length == 1 ) {
+
+			$( document ).ready( function() {
+				var $fixture = $( '#qunit-fixture' );
+				if ( $fixture.length ) {
+					QUnit.config.fixture = $fixture.html();
+				}
+
+				QUnit.start();
+			} );
+		}
+
+		if ( !dependencies.length ) {
+			if ( callback ) {
+				callback.apply( null, modules );
+			}
+		}
+
+		if ( !modules ) {
+			modules = [];
+		}
+
+		var dependency = dependencies.shift();
+		require( [ dependency ], function( module ) {
+			modules.push( module );
+			requireModules( dependencies, callback, modules );
+		} );
+	}
+
+	( function() {
+
+		var scripts = document.getElementsByTagName( "script" );
+		var script = scripts[ scripts.length - 1 ];
+
+		var deps = script.getAttribute( "data-deps" );
+
+		if ( deps ) {
+			deps = deps.replace( /^\s+|\s+$/g, "" ).split( /\s+/ );
+		} else {
+			deps = [];
+		}
+
+		var baseUrl = script.getAttribute( "data-base-url" );
+		var modules = script.getAttribute( "data-modules" );
+		// Format modules attribute
+		if ( modules ) {
+			modules = modules
+					  .replace( /^\s+|\s+$/g, "" )
+					  .split( /\s+/ )
+					  .map( function( module ) {
+					  	// Change to make sure it is loaded from the local folder
+					  	return "./" + module + ".js";
+					  });
+		} else {
+			modules = [];
+		}
+
+		// Load QUnit first among all of them
+		// deps = concat( deps );
+
+		deps = deps.concat( modules );
+
+		require( {
+			baseUrl: baseUrl || "../../../js"
+		} );
+
+		require( [ "qunit" ], function () {		
+			QUnit.config.autostart = false;
+			requireModules( deps, function() {
+			} );
+		})
+
+	} )();
+} )();
